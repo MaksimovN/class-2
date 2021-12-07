9eeec078bbb7f6608ffb74870f137e022e292b13
MaksimovN <91114938+MaksimovN@users.noreply.github.com>
2021-12-07 10:52:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -22,14 +22,24 @@ app.get('/login', (req, res) => {
   res.send('Maksimov');
 });
 
-app.get('/login/ru', (req, res) => {
-  res.set({ 'Content-Type': 'text/html; charset=UTF-8' });
-  res.send('<html><head><meta charset="utf-8"><style>p {font-weight: bold; font-size: 14pt;}</style></head> <body><p>Максимов</p></body></html>')
+app.get('/login/1', (req, res) => {
+  res.set({ 'Content-Type': 'application/json;});
+  res.send('Maksimov');
 });
 
-app.get('/login/by', (req, res) => {
+app.get('/login/2', (req, res) => {
+  res.set({ 'Content-Type': 'application/json; charset=UTF-8' });
+  res.send('Maksimov');
+});
+
+app.get('/login/code1', (req, res) => {
+  res.set({ 'Content-Type': 'text/html; charset=UTF-8' });
+  res.send('<html><head><meta charset="utf-8"><style>p {font-weight: bold; font-size: 21pt;}</style></head> <body><p>Maksimov</p></body></html>')
+});
+
+app.get('/login/code2', (req, res) => {
   res.set({ 'Content-Type': 'text/plain; charset=UTF-8' });
-  res.send('<html><head><meta charset="utf-8"><style>p {font-weight: bold; font-size: 14pt;}</style></head> <body><p>Максимов</p></body></html>')
+  res.send('<html><head><meta charset="utf-8"><style>p {font-weight: bold; font-size: 21pt;}</style></head> <body><p>Maksimov</p></body></html>')
 });
 
 app.get('/promise', (req, res) => {
@@ -51,8 +61,14 @@ app.get('/fetch', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
+app.get('/', (req, res) => {
+  res.statusCode = 404
+  res.send('<html><head><meta charset="utf-8"><style>p {font-weight: bold; font-size: 14pt; color: red;} body{background-color: blue;}</style></head><body><p>Ужас - ошибка 404 (не найдено)</p></body></html>');
+});
+
 app.get('*', (req, res) => {
-  res.send('<html><head><meta charset="utf-8"><style>p {font-weight: bold; font-size: 14pt; color: red;} body{background-color: yellow;}</style></head><body><p>ошибка 404</p></body></html>');
+  res.statusCode = 500
+  res.send('<html><head><meta charset="utf-8"><style>p {font-weight: bold; font-size: 14pt; color: red;} body{background-color: yellow;}</style></head><body><p>Не понял - ошибка  500 (Internal Server Error)</p></body></html>');
 });
 
 app.listen(port);
